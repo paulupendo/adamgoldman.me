953320a87dee415ae9232cbd787e5adc3667a418
goldylucks <adamgoldman7@gmail.com>
2018-03-09 21:35:49 +0700

    tutorial generator temp

--- src/routes/tutorialGenerator/TutorialGenerator.js
+++ src/routes/tutorialGenerator/TutorialGenerator.js
@@ -4,6 +4,9 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import axios from 'axios'
 import FA from 'react-fontawesome'
+import cx from 'classnames'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faKeyboard from '@fortawesome/fontawesome-free-solid/faKeyboard'
 
 import { inputChange, inputToggle } from '../../forms'
 import { reorder, scrollToElem } from '../../utils'
@@ -23,7 +26,7 @@ class TutorialGenerator extends React.Component {
     description: '',
     nick: '',
     credits: '',
-    steps: [],
+    steps: [stepInitialState()],
   }
 
   componentWillMount() {
@@ -32,16 +35,26 @@ class TutorialGenerator extends React.Component {
     }
   }
 
+  componentDidMount() {
+    window.addEventListener('keydown', this.onKeyDown)
+  }
+
+  componentWillUnmount() {
+    window.addEventListener('keydown', this.onKeyDown)
+  }
+
   render() {
     return (
-      <div>
+      <div style={{ padding: 10 }}>
         <div className="clearfix" style={{ width: '60%', float: 'left' }}>
           {this.renderDetails()}
           <hr />
           {this.renderSteps()}
           <a onClick={this.addStep}>+ Step</a>
           <div className={s.controls}>
-            <a className={s.control} onClick={this.save}>Save</a>
+            <a className={s.control} href={`/tools/${this.props.url}`} target="_blank"><FA name="eye" /></a>
+            <a className={s.control} onClick={this.save}><FA name="save" /></a>
+            <a className={s.control} onClick={this.del}><FA name="trash" /></a>
           </div>
         </div>
         <div className="clearfix" style={{ width: '35%', right: 0, position: 'fixed' }}>
@@ -75,7 +88,7 @@ class TutorialGenerator extends React.Component {
           <input className="form-control" placeholder="Credits" value={credits} onChange={inputChange.call(this, 'credits')} />
         </div>
         <div className="form-group">
-          <input type="checkbox" id="isDraft" value={isDraft} checked={isDraft} onChange={inputToggle.call(this, 'isDraft')} />
+          <input style={{ marginRight: 10 }} type="checkbox" id="isDraft" value={isDraft} checked={isDraft} onChange={inputToggle.call(this, 'isDraft')} />
           <label htmlFor="isDraft">Draft</label>
         </div>
       </div>
@@ -95,12 +108,12 @@ class TutorialGenerator extends React.Component {
                 ref={provided.innerRef}
                 style={getListStyle(snapshot.isDraggingOver)}
               >
-                {this.state.steps.map(({ title }, idx) => (
-                  <Draggable key={idx} draggableId={idx}>
+                {this.state.steps.map(({ title }, sIdx) => (
+                  <Draggable key={sIdx} draggableId={sIdx}>
                     {(providedInner, snapshotInner) => (
                       <div>
                         <div
-                          key={idx}
+                          key={sIdx}
                           ref={providedInner.innerRef}
                           style={getItemStyle(
                             providedInner.draggableStyle,
@@ -108,7 +121,7 @@ class TutorialGenerator extends React.Component {
                           )}
                           {...providedInner.dragHandleProps}
                         >
-                          <a onClick={() => scrollToElem(document.querySelector('html'), document.querySelector(`#step-${idx}`).getBoundingClientRect().top - document.body.getBoundingClientRect().top, 300)}>Step {(idx <= 9 && stepsCount > 9) ? `0${idx}` : idx}/{stepsCount}</a> - {title}
+                          <a onClick={() => scrollToElem(document.querySelector('html'), document.querySelector(`#step-${sIdx}`).getBoundingClientRect().top - document.body.getBoundingClientRect().top, 300)}>Step {(sIdx <= 9 && stepsCount > 9) ? `0${sIdx}` : sIdx}/{stepsCount}</a> - {title}
                         </div>
                         {providedInner.placeholder}
                       </div>
@@ -129,11 +142,11 @@ class TutorialGenerator extends React.Component {
       <div key={sIdx} id={`step-${sIdx}`}>
         <div className="row">
           <div className="col-10">
-            <input style={{ width: '100%', border: 0 }} className="h1" id={`step-${sIdx}-title`} placeholder="Title" value={step.title} onChange={this.changeStepKey('title', sIdx)} />
-            <textarea style={{ minHeight: 200, width: '100%', border: 0 }} id={`step-${sIdx}-description`} required className="form-control" placeholder="Description" value={step.description} onChange={this.changeStepKey('description', sIdx)} />
+            <input style={{ width: '100%', border: 0 }} className="h2" id={`step-${sIdx}-title`} placeholder="Step title" value={step.title} onChange={this.changeStepKey('title', sIdx)} />
+            <textarea style={{ minHeight: 200, width: '100%', border: 0 }} id={`step-${sIdx}-description`} required className="form-control" placeholder="Step description" value={step.description} onChange={this.changeStepKey('description', sIdx)} />
           </div>
           <div className="col-2">
-            <p className="text-right">{sIdx}/{this.state.steps.length}</p>
+            <p className="text-right">{sIdx}/{this.state.steps.length - 1} <a onClick={this.removeStep(sIdx)}><FA name="trash-o" /></a></p>
             <select
               className="select"
               style={{ marginRight: 5 }}
@@ -146,7 +159,6 @@ class TutorialGenerator extends React.Component {
               <option value="short">Short</option>
               <option value="long">Long</option>
             </select>
-            <a onClick={this.removeStep(sIdx)}><FA name="trash-o" /></a>
           </div>
         </div>
         <div className="form-group">
@@ -176,32 +188,36 @@ class TutorialGenerator extends React.Component {
     }
     return (
       <div>
-        <a onClick={this.addAnswer(sIdx)}>+ answer</a>
         {step.answers.map((a, aIdx) => (
-          <div style={{ marginLeft: 10 }}>
-            <div className="form-row">
-              <div className="col-auto">
-                <label htmlFor={`step-${sIdx}-answer-${aIdx}`} className="col-form-label"><strong>Answer {aIdx}</strong></label>
+          <div>
+            <div className={cx('row', s.answer)}>
+              <div className="col-10">
+                <input
+                  onKeyPress={(evt) => { this.answerKeyPress(evt, sIdx, aIdx) }}
+                  ref={(el) => { this[`step-${sIdx}-answer-${aIdx}`] = el }}
+                  className="btn btn-primary btn-block text-left"
+                  placeholder={`answer #${aIdx}`}
+                  value={a.text}
+                  onChange={this.changeAnswerKey('text', sIdx, aIdx)}
+                />
               </div>
-              <div className="col-8">
-                <input id={`step-${sIdx}-answer-${aIdx}`} className="form-control" placeholder={`answer #${aIdx}`} value={a.text} onChange={this.changeAnswerKey('text', sIdx, aIdx)} />
-              </div>
-              <div className="col-auto">
-                <a onClick={this.removeAnswer(sIdx, aIdx)}><FA name="trash-o" /></a>
+              <div className={cx('col-2 text-right', s.answerActions)}>
+                <FA onClick={this.toggleAnswerSettings(sIdx, aIdx)} name="cog" className={cx(s.answerSettingsToggle, { [s.isActive]: a.showSettings })} />
+                <FA onClick={this.removeAnswer(sIdx, aIdx)} name="trash-o" />
               </div>
             </div>
-            {[{ id: 'hasGoToStep', text: '--> Step' }, { id: 'isSetInput', text: 'Sets Input' }, { id: 'hasResetInputs', text: 'Resets Input' }, { id: 'isLink', text: 'Link' }, { id: 'isLinkNew', text: 'Link (new window)' }]
-              .map(({ id, text }) => (
+            {a.showSettings && [{ id: 'hasGoToStep', icon: 'paper-plane' }, { id: 'isSetInput', icon: faKeyboard }, { id: 'hasResetInputs', icon: 'eraser' }, { id: 'isLink', icon: 'Link' }, { id: 'link', icon: 'external-link' }]
+              .map(({ id, icon }) => (
                 <div className="form-check form-check-inline">
                   <label htmlFor={`step-${sIdx}-answer-${aIdx}-${id}`} className="form-check-label">
                     <input type="checkbox" className="form-check-input" id={`step-${sIdx}-answer-${aIdx}-${id}`} value={a[id]} checked={a[id]} onChange={this.toggleAnswerKey(id, sIdx, aIdx)} />
-                    {text}
+                    <FontAwesomeIcon icon={icon} />
                   </label>
                 </div>
                 ))}
             <div className="form-group">
-              { a.isSetInput && <input placeholder="id" value={a.inputId} onChange={this.changeAnswerKey('inputId', sIdx, aIdx)} /> }
-              { a.isSetInput && <input placeholder="value" value={a.inputValue} onChange={this.changeAnswerKey('inputValue', sIdx, aIdx)} /> }
+              { a.isSetInput && <input placeholder="Input id" value={a.inputId} onChange={this.changeAnswerKey('inputId', sIdx, aIdx)} /> }
+              { a.isSetInput && <input placeholder="Input value" value={a.inputValue} onChange={this.changeAnswerKey('inputValue', sIdx, aIdx)} /> }
             </div>
             <div className="form-group">
               { a.hasGoToStep && <input placeholder="title" value={a.goToStepByTitle} onChange={this.changeAnswerKey('goToStepByTitle', sIdx, aIdx)} /> }
@@ -218,9 +234,9 @@ class TutorialGenerator extends React.Component {
             <div className="form-group">
               { a.isLinkNew && <input placeholder="path" value={a.linkNew} onChange={this.changeAnswerKey('linkNew', sIdx, aIdx)} /> }
             </div>
-            { step.answers.length - 1 > aIdx && <hr className={s.answersHr} />}
           </div>
           ))}
+        <a onClick={this.addAnswer(sIdx)}>+ answer</a>
       </div>
     )
   }
@@ -242,75 +258,68 @@ class TutorialGenerator extends React.Component {
     })
   }
 
-  removeStep = idx => () => {
+  removeStep = sIdx => () => {
     const nextSteps = [...this.state.steps]
-    nextSteps.splice(idx, 1)
+    nextSteps.splice(sIdx, 1)
     this.setState({ steps: nextSteps })
   }
 
   addStep = () => {
     const nextSteps = [...this.state.steps]
-    nextSteps.push({
-      title: '',
-      description: '',
-      type: 'radio',
-      inputId: '',
-      inputPlaceholder: '',
-      answers: [],
-    })
+    nextSteps.push(stepInitialState())
     this.setState({ steps: nextSteps })
   }
 
-  changeStepKey = (key, idx) => (evt) => {
+  changeStepKey = (key, sIdx) => (evt) => {
     const nextSteps = [...this.state.steps]
-    nextSteps[idx][key] = evt.target.value
+    nextSteps[sIdx][key] = evt.target.value
     this.setState({ steps: nextSteps })
   }
 
-  toggleStepKey = (key, idx) => () => {
+  toggleStepKey = (key, sIdx) => () => {
     const nextSteps = [...this.state.steps]
-    nextSteps[idx][key] = !nextSteps[idx][key]
+    nextSteps[sIdx][key] = !nextSteps[sIdx][key]
     this.setState({ steps: nextSteps })
   }
 
-  changeAnswerKey = (key, idx, aIdx) => (evt) => {
+  changeAnswerKey = (key, sIdx, aIdx) => (evt) => {
     const nextSteps = [...this.state.steps]
-    nextSteps[idx].answers[aIdx][key] = evt.target.value
+    nextSteps[sIdx].answers[aIdx][key] = evt.target.value
     this.setState({ steps: nextSteps })
   }
 
-  toggleAnswerKey = (key, idx, aIdx) => () => {
+  toggleAnswerKey = (key, sIdx, aIdx) => () => {
     const nextSteps = [...this.state.steps]
-    nextSteps[idx].answers[aIdx][key] = !nextSteps[idx].answers[aIdx][key]
+    nextSteps[sIdx].answers[aIdx][key] = !nextSteps[sIdx].answers[aIdx][key]
     this.setState({ steps: nextSteps })
   }
 
-  addAnswer = idx => () => {
+  addAnswer = sIdx => () => {
     const nextSteps = [...this.state.steps]
-    nextSteps[idx].answers.push({
-      text: '',
-      alert: '',
-      hasAlert: false,
-      isSetInput: false,
-      inputId: '',
-      inputValue: '',
-      hasResetInputs: '',
-      resetInputs: '',
-      hasGoToStep: false,
-      goToStepByTitle: '',
-      isFbShare: false,
-      isLink: false,
-      link: '',
-      isLinkNew: false,
-      linkNew: '',
-    })
+    nextSteps[sIdx].answers.push(answerInitialState())
     this.setState({ steps: nextSteps })
   }
 
-  removeAnswer = (idx, aIdx) => () => {
+  removeAnswer = (sIdx, aIdx) => () => {
     const nextSteps = [...this.state.steps]
-    nextSteps[idx].answers.splice(aIdx, 1)
+    nextSteps[sIdx].answers.splice(aIdx, 1)
     this.setState({ steps: nextSteps })
+  }
+
+  toggleAnswerSettings = (sIdx, aIdx) => () => {
+    const nextSteps = [...this.state.steps]
+    nextSteps[sIdx].answers[aIdx].showSettings = !nextSteps[sIdx].answers[aIdx].showSettings
+    this.setState({ steps: nextSteps })
+  }
+
+  answerKeyPress = (evt, sIdx, aIdx) => {
+    if (evt.key !== 'Enter') {
+      return
+    }
+    const nextSteps = [...this.state.steps]
+    nextSteps[sIdx].answers.splice(aIdx + 1, 0, answerInitialState())
+    evt.preventDefault()
+    this.setState({ steps: nextSteps }, () => this[`step-${sIdx}-answer-${aIdx + 1}`].focus())
   }
 
   save = () => {
@@ -327,6 +336,29 @@ class TutorialGenerator extends React.Component {
         global.console.error(err)
         global.alert(err.message)
       })
+  }
+
+  del = () => {
+    if (!global.confirm('Sure you want to delete this tool?')) {
+      return
+    }
+    axios.post('/api/tools/', { url: this.props.url })
+      .then((res) => {
+        global.console.log('deleted!', res.data)
+        global.alert('deleted!')
+      })
+      .catch((err) => {
+        global.console.error(err)
+        global.alert(err.message)
+      })
+  }
+  onKeyDown = () => {
+    // if (evt.srcElement.getAttribute('data-answer')) {
+    //   this.addAnswerToStep(evt.srcElement)
+    //   return
+    // }
+    // console.log(evt)
+    // console.log(evt.key)
   }
 }
 
@@ -377,5 +409,37 @@ function getListStyle(isDraggingOver) {
   return {
     background: isDraggingOver ? 'lightblue' : 'lightgrey',
     padding: grid,
+  }
+}
+
+function stepInitialState() {
+  return {
+    title: '',
+    description: '',
+    type: 'radio',
+    inputId: '',
+    inputPlaceholder: '',
+    answers: [answerInitialState()],
+  }
+}
+
+function answerInitialState() {
+  return {
+    showSettings: false,
+    text: '',
+    alert: '',
+    hasAlert: false,
+    isSetInput: false,
+    inputId: '',
+    inputValue: '',
+    hasResetInputs: '',
+    resetInputs: '',
+    hasGoToStep: false,
+    goToStepByTitle: '',
+    isFbShare: false,
+    isLink: false,
+    link: '',
+    isLinkNew: false,
+    linkNew: '',
   }
 }
